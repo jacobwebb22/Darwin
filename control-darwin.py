5dc325ebdd7e46ed232bb8c221d4c9fa8d67f9b1
Jacob Webb rpi <jacob.webb22@gmail.com>
2015-04-11 18:07:51 -0400

    added keypress detection and mapped to darwin controls

--- control-darwin.py
+++ control-darwin.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python
 
 import serial
-import binascii
+import os
+import sys
+import termios
+import tty
+
+# Initialize Serial Port
 
 port = serial.Serial("/dev/ttyAMA0", baudrate = 57600, timeout = 2)
-
-#sit = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 03 00 d9 ed'.split()))
-#stand = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 04 00 da 7f'.split()))
-#forward = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 13 00 da 0d'.split()))
-#reverse = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 14 00 d9 9f'.split()))
-#sideleft = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 12 00 d9 8b'.split()))
-#sideright = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 11 00 d9 81'.split()))
-#turnleft = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 10 00 da 07'.split()))
-#turnright = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 0f 00 d9 c5'.split()))
-#initial = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 0f 00 d9 c5'.split()))
-#waveleft = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 0e 00 da 43'.split()))
-#waveright = binascii.unhexlify(''.join('ff ff fd 00 c8 07 00 03 42 00 0d 00 da 49'.split()))
-
-#port.write(b'\xb0\xb1')
-#port.close()
 
 sit = (b'\xff\xff\xfd\x00\xc8\x07\x00\x03\x42\x00\x03\x00\xd9\xed')
 stand = (b'\xff\xff\xfd\x00\xc8\x07\x00\x03\x42\x00\x04\x00\xda\x7f')
@@ -32,5 +22,47 @@ initial = (b'\xff\xff\xfd\x00\xc8\x07\x00\x03\x42\x00\x01\x00\xda\x61')
 waveleft = (b'\xff\xff\xfd\x00\xc8\x07\x00\x03\x42\x00\x0e\x00\xda\x43')
 waveright = (b'\xff\xff\xfd\x00\xc8\x07\x00\x03\x42\x00\x0d\x00\xda\x49')
 
-port.write(waveright)
-port.close()
+# Get Keypress Function
+
+def getKey():
+   fd = sys.stdin.fileno()
+   old = termios.tcgetattr(fd)
+   new = termios.tcgetattr(fd)
+   new[3] = new[3] & ~termios.ICANON & ~termios.ECHO
+   new[6][termios.VMIN] = 1
+   new[6][termios.VTIME] = 0
+   termios.tcsetattr(fd, termios.TCSANOW, new)
+   key = None
+   try:
+      key = os.read(fd, 3)
+   finally:
+      termios.tcsetattr(fd, termios.TCSAFLUSH, old)
+   return key
+
+# Main Loop
+
+while 1:
+	x = str(getKey())
+	if x == "p":
+		port.close()
+		break
+	if x == "i":
+		port.write(initial)
+	if x == "w":
+		port.write(forward)
+	if x == "a":
+		port.write(sideleft)
+	if x == "s":
+		port.write(sideright)
+        if x == "d":
+                port.write(forward)
+        if x == "q":
+                port.write(turnleft)
+        if x == "r":
+                port.write(turnright)
+        if x == "t":
+                port.write(waveleft)
+        if x == "y":
+                port.write(waveright)
+#        if x == "":
+#                port.write()
